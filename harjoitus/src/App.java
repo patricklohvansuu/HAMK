25a690720f336b5ec43ada05ac8afe8450502ead
patricklohvansuu <patrick.lohvansuu@gmail.com>
2026-09-19 12:42:12 +0300

    Lisätty luku1-muuttuja ja annettu sille arvo 1.

--- harjoitus/src/App.java
+++ harjoitus/src/App.java
@@ -5,5 +5,9 @@ public class App {
         String tekija;
         tekija = "Patrick";
         System.out.println("Ohjelman tekijä: " + tekija);
+        int luku1;
+        luku1 = 1;
+        System.out.println("luku1-muuttujan arvo on " + luku1);
+
     }
 }
