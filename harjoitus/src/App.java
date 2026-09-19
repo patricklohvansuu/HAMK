b80e809b4d30162766a5325bbfe1126d0727aab9
patricklohvansuu <patrick.lohvansuu@gmail.com>
2026-09-19 12:25:14 +0300

    Lisätty tekijän nimi String-tyyppisenä muuttujana.

--- harjoitus/src/App.java
+++ harjoitus/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hei olen Tulostin-ohjelma");
+        
+        String tekija;
+        tekija = "Patrick";
+        System.out.println("Ohjelman tekijä: " + tekija);
     }
 }
